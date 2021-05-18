746944a4845bc8432045308296967a744c32f716
Hamzah <hamzahullah@yahoo.com>
2021-05-18 13:36:29 -0400

    added code to match snapshot code

--- assets/js/script.js
+++ assets/js/script.js
@@ -72,6 +72,14 @@ $(".list-group").on("blur", "textarea", function () {
 
     tasks[status][index].text = text;
     saveTasks();
+
+      // recreate p element
+  var taskP = $("<p>")
+  .addClass("m-1")
+  .text(text);
+
+// replace textarea with new content
+$(this).replaceWith(taskP);
 });
 // due date was clicked
 $(".list-group").on("click", "span", function() {
